9bd4bff12e86560231e561da484261b883eb23e9
Sherry Bai <sherrybai01@gmail.com>
2025-01-20 12:02:47 -0500

    refactor + fix tests

--- src/codegen/mod.rs
+++ src/codegen/mod.rs
@@ -2,7 +2,7 @@ use anyhow::anyhow;
 #[cfg(test)]
 use assert_str::assert_str_trim_eq;
 
-use crate::parser::AstNode;
+use crate::parser::{AstNode, Operator};
 
 const INDENT: &str = "	";
 
@@ -20,6 +20,13 @@ pub fn codegen(ast: AstNode) -> anyhow::Result<String> {
     Ok(result)
 }
 
+fn format_instruction(instruction: &str, args: Vec<&str>) -> String {
+    if args.len() > 0 {
+        format!("{}{}{}{}\n", INDENT, instruction, INDENT, args.join(", "))
+    } else {
+        format!("{}{}\n", INDENT, instruction)
+    }
+}
 fn generate_function(node: AstNode) -> anyhow::Result<String> {
     let AstNode::Function{identifier, statement} = node else {
         return Err(anyhow!("Called generate_function on node that is not a function"))
@@ -45,12 +52,43 @@ fn generate_statement(node: AstNode) -> anyhow::Result<String> {
     // only return statements supported for now
     let mut result: String = String::new();
 
-    let AstNode::Constant{constant} = *expression else {
-        return Err(anyhow!("Malformed expression"))
-    };
-    result.push_str(&format!("{}mov{}w0, #{}\n", INDENT, INDENT, constant));
-    result.push_str(&format!("{}ret\n", INDENT));
+    let generated_expression: String = generate_expression(*expression)?;
+    result.push_str(&generated_expression);
+    result.push_str(&format_instruction("ret", vec![]));
 
+    Ok(result)
+}
+
+fn generate_expression(node: AstNode) -> anyhow::Result<String> {
+    let mut result: String = String::new();
+
+    match node {
+        AstNode::Constant { constant } => {
+            let constant_str = format!("#{}", constant);
+            result.push_str(&format_instruction("mov", vec!["w0", &constant_str[..]]));
+        },
+        AstNode::UnaryOp { operator, expression } => {
+            let nested_expression = generate_expression(*expression)?;
+            result.push_str(&nested_expression);
+            // assume previous operation moves the value to w0
+            match operator {
+                Operator::Negation => {
+                    result.push_str(&format_instruction("neg", vec!["w0", "w0"]));
+                },
+                Operator::BitwiseComplement => {
+                    result.push_str(&format_instruction("mvn", vec!["w0", "w0"]));  // logical nor with itself
+                },
+                Operator::LogicalNegation => {
+                    result.push_str(&format_instruction("cmp", vec!["w0", "#0"]));  // compare w0 to 0, setting flag
+                    result.push_str(&format_instruction("mov", vec!["w0", "#1"]));  // set w0 to 1 (doesn't clear flags)
+                    result.push_str(&format_instruction("csel", vec!["w0", "w0", "wzr", "EQ"]));  // select 1 if true else 0
+                }
+            }
+        }
+        _ => {
+            return Err(anyhow!("Malformed expression"))
+        }
+    }
     Ok(result)
 }
 
@@ -71,6 +109,26 @@ mod tests {
                 .globl _main
                 _main:
                     mov	w0, #2
+                    ret
+            "
+        )
+    }
+
+    #[test]
+    fn test_unary_op() {
+        let constant = Box::new(AstNode::Constant { constant: 2 });
+        let expression = Box::new(AstNode::UnaryOp { operator: Operator::BitwiseComplement, expression: constant });
+        let statement = Box::new(AstNode::Statement {expression});
+        let function = Box::new(AstNode::Function {identifier: "main".into(), statement});
+        let program = AstNode::Program{function};
+
+        let result = codegen(program).unwrap();
+        assert_str_trim_eq!(
+            result, "
+                .globl _main
+                _main:
+                    mov	w0, #2
+                    mvn	w0, w0
                     ret
             "
         )
